fef5e33c6df9cf48f986f627873e92ef913ca6ed
Greg Hawthorne <ghawthorne@me.com>
2020-11-24 07:31:59 +0100

    Complete UI

--- main.py
+++ main.py
@@ -21,10 +21,23 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.config(padx=100, pady=50, bg=YELLOW)
 window.title("Pomodoro")
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔︎", bg=YELLOW, fg=GREEN, pady=15, font=(FONT_NAME, 36))
+check_marks.grid(column=1, row=2)
 
 window.mainloop()
